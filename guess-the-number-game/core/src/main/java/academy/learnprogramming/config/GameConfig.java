6411568efcaf259c4837b9f244894fbf65624994
StepanB <stepan.bohm@expertkom.cz>
2020-08-13 22:24:20 +0200

    Multi-module Spring Project

---
+++ guess-the-number-game/core/src/main/java/academy/learnprogramming/config/GameConfig.java
@@ -0,0 +1,43 @@
+package academy.learnprogramming.config;
+
+import academy.learnprogramming.GuessCount;
+import academy.learnprogramming.MaxNumber;
+import academy.learnprogramming.MinNumber;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.*;
+
+@Configuration
+@ComponentScan(basePackages = "academy.learnprogramming")
+@PropertySource("classpath:config/game.properties")
+public class GameConfig {
+
+    // == fields ==
+    @Value("${game.maxNumber:20}")
+    private int maxNumber;
+
+    @Value("${game.minNumber:0}")
+    private int minNumber;
+
+    @Value("${game.guessCount:5}")
+    private int guessCount;
+
+
+    // == bean methods ==
+    @Bean
+    @MaxNumber
+    public int getMaxNumber() {
+        return maxNumber;
+    }
+
+    @Bean
+    @GuessCount
+    public int getGuessCount() {
+        return guessCount;
+    }
+
+    @Bean
+    @MinNumber
+    public int getMinNumber() {
+        return minNumber;
+    }
+}
