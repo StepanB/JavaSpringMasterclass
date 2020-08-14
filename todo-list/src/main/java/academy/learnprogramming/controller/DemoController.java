9e9ad1ce632b323d5aa5e2408ad0b666182c0158
StepanB <stepan.bohm@expertkom.cz>
2020-08-14 23:47:58 +0200

    Service Challenge

--- todo-list/src/main/java/academy/learnprogramming/controller/DemoController.java
+++ todo-list/src/main/java/academy/learnprogramming/controller/DemoController.java
@@ -1,11 +1,25 @@
 package academy.learnprogramming.controller;
 
+import academy.learnprogramming.DemoService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+@Slf4j
 @Controller
 public class DemoController {
+
+    // == fields ==
+    private final DemoService demoService;
+
+    // == constructor ==
+    @Autowired
+    public DemoController(DemoService demoService) {
+        this.demoService = demoService;
+    }
 
     @ResponseBody
     @GetMapping("/hello")
@@ -14,7 +28,11 @@ public class DemoController {
     }
 
     @GetMapping("welcome")
-    public String welcome() {
+    public String welcome(Model model) {
+        model.addAttribute("helloMessage", demoService.getHelloMessage("Tim"));
+        model.addAttribute("welcomeMessage", demoService.getWelcomeMessage());
+//        log.info("model= {}", model);
         return "welcome";
     }
+
 }
